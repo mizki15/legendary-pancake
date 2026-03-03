0efd5942d092ea71e2725a33ae4838206d0596a4
Mizki <152066161+mizki15@users.noreply.github.com>
2026-03-03 22:12:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 # 1. 各Blueprintをインポート
 from routes.study import study_bp
 from routes.work_optimize1 import work_optimize1_bp
+from routes.work_optimize1 import work_optimize2_bp
 from routes.rocket import rocket_bp
 from routes.misc import misc_bp
 
@@ -22,12 +23,14 @@ def index_top():
 # =========================
 app.register_blueprint(study_bp)
 app.register_blueprint(work_optimize1_bp)
+app.register_blueprint(work_optimize2_bp)
 app.register_blueprint(rocket_bp)
 app.register_blueprint(misc_bp)
 
 if __name__ == '__main__':
     # 開発環境ではdebug=True
     app.run(debug=True)
+
 
 
 
