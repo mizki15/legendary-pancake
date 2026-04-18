90acc451d9d2b123c94dab01d75afc86ddd9bf28
Mizki <152066161+mizki15@users.noreply.github.com>
2026-04-18 22:46:38 +0900

    Add ut_eitan_quiz blueprint and secret key
    Added a new blueprint for the ut_eitan_quiz module and set a secret key for the Flask app.

--- app.py
+++ app.py
@@ -7,11 +7,15 @@ from routes.work_optimize1 import work_optimize1_bp
 from routes.work_optimize2 import work_optimize2_bp
 from routes.rocket import rocket_bp
 from routes.misc import misc_bp
+from routes.ut_eitan_quiz import ut_eitan_quiz_bp
 
 # .env読み込み
 load_dotenv()
 
 app = Flask(__name__)
+
+app.secret_key = "secret_key"
+
 
 # インデックス（トップページ）
 @app.route("/")
@@ -29,9 +33,17 @@ app.register_blueprint(work_optimize2_bp, url_prefix="/opt2")
 app.register_blueprint(rocket_bp)
 app.register_blueprint(misc_bp)
 
+app.register_blueprint(ut_eitan_quiz_bp)
+
 if __name__ == '__main__':
     # 開発環境ではdebug=True
     app.run(debug=True)
+
+
+# ここが変更点
+
+
+
 
 
 
