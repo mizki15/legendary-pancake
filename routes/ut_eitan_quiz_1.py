c81b40aa4c141c204be0c5a263bbdc188bab63e5
Mizki <152066161+mizki15@users.noreply.github.com>
2026-05-24 22:12:13 +0900

    Update ut_eitan_quiz_1.py

--- routes/ut_eitan_quiz_1.py
+++ routes/ut_eitan_quiz_1.py
@@ -151,7 +151,7 @@ def quiz_home():
     
     # 4. sidebar_tree を追加してレンダリング
     return render_template(
-        'ut_eitan_quiz/quiz_1.html',
+        'ut_eitan_quiz_1/quiz.html',
         sentence_template=replaced_sentence,
         hints=hint_list,
         targets_count=len(targets),
